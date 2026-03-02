6f1dbd147a29d617438f4dcc63343cd09a5af091
knowledgebuddy2047 <knowledgebuddy2047@gmail.com>
2026-03-03 01:13:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -270,9 +270,9 @@ function renderFlashcard() {
       </div>
     </div>
     <div class="controls">
-      <button onclick="prevFlashcard()"><img src="images/previous.png" class="icon" alt="Prev"></button>
-      <button onclick="nextFlashcard()"><img src="images/next.png" class="icon" alt="Next"></button>
-      <button onclick="shuffleFlashcards()"><img src="images/shuffle.png" class="icon" alt="Shuffle"></button>
+      <button onclick="prevFlashcard()"><img src="data/images/previous.png" class="icon" alt="Prev"></button>
+      <button onclick="nextFlashcard()"><img src="data/images/next.png" class="icon" alt="Next"></button>
+      <button onclick="shuffleFlashcards()"><img src="data/images/shuffle.png" class="icon" alt="Shuffle"></button>
     </div>
     <div class="progress">Card ${flashIndex + 1} of ${flashcardsData.length}</div>
   `;
@@ -303,6 +303,7 @@ function shuffleFlashcards() {
   flashFlipped = false;
   renderFlashcard();
 }
+
 
 
 
